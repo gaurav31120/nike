b7ad6afe47d35d63ee77f76f3604940076276fca
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-02 09:16:16 +0530

    wide spelling mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => (
   <main className="relative">
     Nav
-    <section className="xl:padding-1 wode:padding-r padding-b">
+    <section className="xl:padding-1 wide:padding-r padding-b">
       Hero
     </section>
     <section className="padding">
