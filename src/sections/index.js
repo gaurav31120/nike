ff2d43471d8693fbd41b4dd5fcd213e677a8d60d
Gaurav Kumar <gaurav31120@gmail.com>
2024-04-06 11:00:13 +0530

    Creating Components

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+
+export { Hero, PopularProducts, SuperQuality, Services,Subscribe,SpecialOffer,Footer,CustomerReviews };
